ed4fd6a9d0c79e15c18ee8eb17d9c898df4e2d2c
fiqrian <fiqrian.faturahman@gmail.com>
2025-10-21 09:31:05 +0700

    Update aztec-installer.sh

--- Aztec-Installer/aztec-installer.sh
+++ Aztec-Installer/aztec-installer.sh
@@ -314,7 +314,7 @@ services:
       options: { max-size: "10m", max-file: "3" }
 
   prysm:
-    image: gcr.io/prysmaticlabs/prysm/beacon-chain:v6.1.2  
+    image: gcr.io/prysmaticlabs/prysm/beacon-chain:stable 
     container_name: prysm
     network_mode: host
     restart: unless-stopped
